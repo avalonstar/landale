deba94c32d3563a5bc08e80fee565db338b1ab82
Bryan Veloso <bryan@avalonstar.com>
2020-09-28 09:19:53 -0700

    refactor: Monorepo the project.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,16 +7,22 @@ import { Connection } from 'typeorm'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmConfigService } from './typeorm.config'
 import { QuotesModule } from './quotes/quotes.module'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
     QuotesModule,
   ],
   controllers: [AppController],
---
+++ apps/api/src/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common'
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+import { Quote } from './quotes/quotes.entity'
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Quote],
+      synchronize: true,
+    }
+  }
+}
